e30b1254b31e73e72e38e92743b5cdeffd262598
Etanaru <Alehin.V.M@mail.com>
2022-12-07 15:20:11 +0400

    Task19

--- HomeWork/HomeWork3/Task19/Program.cs
+++ HomeWork/HomeWork3/Task19/Program.cs
@@ -4,3 +4,16 @@
 // 12821 -> да
 // 23432 -> да
 
+// Вариант подходит как для цифр так и букв.
+
+Console.WriteLine("Введите пятизначное число:  ");
+String number = Console.ReadLine()!;
+
+void Check(string number)
+{
+    if (number[0] == number[4] | number[1] == number[3]) Console.WriteLine($"Это число: {number} - палиндром.");
+    else Console.WriteLine($"Это число: {number} - не палиндром.");
+}
+if (number!.Length == 5) Check(number);
+else Console.WriteLine($"Введите корректное число!");
+
