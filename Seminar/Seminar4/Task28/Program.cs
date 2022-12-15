1f94b4cea6589e184cb80c5f4141540f71a36b1b
Etanaru <Alehin.V.M@mail.com>
2022-12-15 13:14:01 +0400

    renameFolders

---
+++ Seminar/Seminar4/Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");           
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел = {GetPrdctNums(num)}");
+
+
+int GetPrdctNums(int number)
+{
+    int count = 1;
+    while (number> 0)
+    {
+        count*=number;
+        number--;
+    }
+    return count;
+}
