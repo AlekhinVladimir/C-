55340e9488c453871ad95dc03e6f00908f5582fb
Etanaru <Alehin.V.M@mail.com>
2022-12-20 15:46:42 +0400

    Task44

--- Seminar/Seminar6/Task44/Program.cs
+++ Seminar/Seminar6/Task44/Program.cs
@@ -3,12 +3,16 @@
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
-int num = int.Parse(Console.ReadLine()!);
-int[] listFib = new int[num];
+ void MetFib(int Num)
+{
+int[] listFib = new int[Num];
 Console.Write(listFib[0]);
 listFib[1] = 1; Console.Write(listFib[1]);
-for (int i = 2; i < num; i++)
+for (int i = 2; i < Num; i++)
 {
 listFib[i] = listFib[i-1]+listFib[i-2];
 Console.Write(listFib[i]);
 }
+}
+int num = int.Parse(Console.ReadLine()!);
+MetFib(num);
