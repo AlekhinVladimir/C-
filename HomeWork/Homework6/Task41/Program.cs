3aac60d4835582c93a90fcf87122a64526ca5009
Etanaru <Alehin.V.M@mail.com>
2022-12-20 23:11:38 +0400

    Task41

---
+++ HomeWork/Homework6/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void CountGreaterThen0 (string[] numbers)
+{
+    int count = 0;
+    foreach (var digit in numbers)
+    {
+        if (int.Parse(digit) > 0) count++;
+    }
+    Console.WriteLine($"\nЧисел больше 0 -> {count}");
+}
+
+Console.Write("Введите числа через пробел или запятую: ");
+string[] array = (Console.ReadLine()!).Split(' ',',');
+CountGreaterThen0(array);
